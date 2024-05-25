dfdf83b706f90c08e99066ce35303230109a0eae
lenaklaassen <116567623+lenaklaassen@users.noreply.github.com>
2024-05-25 18:01:55 +0200

    Delete public_html directory

--- public_html/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.STEIN1').addEventListener('click',SprechblaseAn);
-		document.querySelector('.button2').addEventListener('click',SprechblaseAus);
-		
-		function SprechblaseAn() {
-			document.querySelector('.STECKBRIEF').classList.add('SICHTBAR');
-		}
-		function SprechblaseAus() {
-			document.querySelector('.STECKBRIEF').classList.remove('SICHTBAR');
-		}
